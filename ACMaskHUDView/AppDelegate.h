2f4c3a205e553dbb56a26eb94e1047437f9a8033
Albert Chu <albertzyk@gmail.com>
2014-07-29 14:05:49 +0800

    Initial Commit

---
+++ ACMaskHUDView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ACMaskHUDView
+//
+//  Created by Albert Chu on 14/7/29.
+//  Copyright (c) 2014年 AC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
